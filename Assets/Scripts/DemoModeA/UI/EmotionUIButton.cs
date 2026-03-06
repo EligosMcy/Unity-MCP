29334a3c2120d9b0d2621166f0f802e2edcc22b2
Eligos <2284226884@qq.com>
2026-03-06 11:34:14 +0800

    修改一下按钮功能的设置

--- Assets/Scripts/DemoModeA/UI/EmotionUIButton.cs
+++ Assets/Scripts/DemoModeA/UI/EmotionUIButton.cs
@@ -1,29 +1,40 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace DemoModeA
 {
     public class EmotionUIButton : MonoBehaviour
     {
         [SerializeField] private Button _button;
+        [SerializeField] private TextMeshProUGUI _text;
         [SerializeField] private EmotionType _emotion;
+        [Space]
         [SerializeField] private MonoBehaviour _controllerSource;
         private IEmotionController _controller;
 
         private void Awake()
         {
-            if (_button == null)
-            {
-                _button = GetComponent<Button>();
-            }
-            _controller = _controllerSource as IEmotionController;
-            if (_controller == null)
-            {
-                _controller = FindFirstObjectByType<EmotionController>();
-            }
             if (_button != null)
             {
                 _button.onClick.AddListener(onClick);
+            }
+
+            _controller = _controllerSource as IEmotionController ?? FindFirstObjectByType<EmotionController>();
+
+            updateText();
+        }
+
+        private void OnValidate()
+        {
+            updateText();
+        }
+
+        private void updateText()
+        {
+            if (_text != null)
+            {
+                _text.text = _emotion.ToString();
             }
         }
 
